8375c757427391cab68f442bc06aea5ddfe060ff
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2024-01-17 01:24:59 -0300

    a

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,63 @@
 <template>
-    <div class="container">
-        <h1>ABOUT</h1>
+    <div>
+        <div class="container d-flex flex-wrap justify-content-between mt-5">
+            <div class="card bg-dark text-white mb-3" style="width: 18rem;">
+                <img class="card-img-top" :src="camara" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Sistemas de seguridad especiales</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+            <div class="card bg-dark text-white mb-3" style="width: 18rem;">
+                <img class="card-img-top" :src="candado" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Sistemas de Seguridad para hogares</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+            <div class="card bg-dark text-white mb-3" style="width: 18rem;">
+                <img class="card-img-top" :src="seguridad" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Sistemas de seguridad para la Oficina</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+import camara from '../assets/page1-img1.jpg'
+import candado from '../assets/page1-img2.jpg'
+import seguridad from '../assets/page1-img3.jpg'
 
 </script>
 
 <style  scoped>
+.container {
+    width: 75%;
+}
+
+.btn{
+    background-color: #343a40;
+}
+
+@media (max-width: 767px) {
+    .container {
+        width: 75%;
+    }
+
+    .card {
+        width: 100%;
+    }
+}
 </style>
--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,6 +1,51 @@
 <template>
-    <div class="container">
+    <div class="container mt-5">
         <h1>How We Recruit</h1>
+        <div class="text-muted">
+            <p class="title">Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat
+            </p>
+
+            Sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+            takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+            diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+            dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+            rebum. Stet clita kasd gubergren, no sea takimata sanctus.
+
+            <p class="title">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper</p>
+
+            Suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in
+            vulputate velit esse molestie consequat, vel illum dolore orem ipsum dolor sit amet, consetetur sadipscing
+            elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+            <p class="title">
+                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet,
+                consetetur</p>
+
+            Sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+            voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+            sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+            justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+            amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua.
+
+            <p class="title">At vero eos et accusam et justo duo dolores et ea rebum</p>
+
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+            diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+            takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+            diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+            dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+        </div>
     </div>
 </template>
 
@@ -8,4 +53,22 @@
 
 </script>
 
-<style  scoped></style>
+<style  scoped>
+.title {
+    color: #e88800;
+}
+
+.container {
+    width: 75%;
+}
+
+.parrafo {
+    color: #c60000;
+}
+
+@media(max-width:768px) {
+    .container {
+        width: 100%;
+    }
+}
+</style>
